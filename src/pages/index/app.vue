49627d6829a33a5dac07e5f8bbe68a82b031e5a0
cfwu <cfwu@vova.com.hk>
2020-03-28 19:51:15 +0800

    first commit

---
+++ src/pages/index/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="index-card">{{msg}}</div>
+  </div>
+</template>
+
+<script>
+import modal from '../../components/modal.vue'
+export default {
+  components: {
+    modal
+  },
+  data() {
+    return {
+      msg: 'Use Vue 2.0 Today!'
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="postcss">
+.index-card {
+  width: 300px;
+  margin: 10px auto;
+  font-size: 30px;
+}
+</style>
